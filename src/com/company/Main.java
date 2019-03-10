70ed96c66611c521d727a1884f5ac751082bd670
Pyr0Joke <shmel9311@gmail.com>
2019-03-10 19:32:09 +0300

    rebase A

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,9 @@ package com.company;
 
 public class Main {
 
+    private final int a = 5;
+
     public static void main(String[] args) {
 	// write your code here
     }
 }
-private final int a = 5
